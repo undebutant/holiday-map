6df8fe338eb52120ce670c31c57f0165939937be
Guillaume Gonthier <guillaume.gonthier@nadeo.com>
2021-08-12 14:16:10 +0200

    Code style to match go fmt conventions

--- main.go
+++ main.go
@@ -1,18 +1,73 @@
 package main
 
 import (
-    //"net/http"
-    "github.com/gin-gonic/gin"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-    // Creates a gin router with default middleware
-    router := gin.Default()
-    
-    // Serves static files
-    router.StaticFile("/", "./resources/main.html")
-    router.StaticFile("/static/main.css", "./resources/main.css")
+// Consts definitions
+const JSON_PATH = "./data/data.json"
 
-    // Listen and serves on localhost:8080
-    router.Run()
+// Structs definitions
+type Data struct {
+	Markers    []Marker `json:"markers"`
+	PhotoCount int      `json:"photoCount"`
+}
+
+type Marker struct {
+	Name      string  `json:"name"`
+	Latitude  float32 `json:"latitude"`
+	Longitude float32 `json:"longitude"`
+	Photos    []Photo `json:"photos"`
+}
+
+type Photo struct {
+	Id          int    `json:"id"`
+	FileName    string `json:"fileName"`
+	Description string `json:"description"`
+	Date        string `json:"date"`
+}
+
+// Route functions
+func getMarkers(context *gin.Context) {
+	jsonData, fileError := os.Open(JSON_PATH)
+
+	if fileError != nil {
+		context.Status(http.StatusInternalServerError)
+		return
+	}
+	defer jsonData.Close()
+
+	byteValue, decodingError := ioutil.ReadAll(jsonData)
+	if decodingError != nil {
+		context.Status(http.StatusInternalServerError)
+		return
+	}
+
+	var data Data
+	json.Unmarshal(byteValue, &data)
+
+	context.JSON(http.StatusOK, data.Markers)
+}
+
+func main() {
+	// Create a gin router with default middleware
+	router := gin.Default()
+
+	// Serve static web files
+	router.StaticFile("/", "./resources/main.html")
+	router.StaticFile("/static/main.css", "./resources/main.css")
+
+	// Serve photos
+	router.Static("/photos", "./data/photos")
+
+	// Dynamic routing
+	router.GET("/markers", getMarkers)
+
+	// Listen and serve on localhost:8080
+	router.Run()
 }
